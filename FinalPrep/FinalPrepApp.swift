cc50b50b7340b94247e50b109d3c0c2889451f2a
Cyrus <cyrushoyat@gmail.com>
2025-04-13 02:35:43 -0700

    Initial Commit

---
+++ FinalPrep/FinalPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalPrepApp.swift
+//  FinalPrep
+//
+//  Created by MosquitoOuO on 2025-04-13.
+//
+
+import SwiftUI
+
+@main
+struct FinalPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinalPrepTests/FinalPrepTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinalPrepTests.swift
+//  FinalPrepTests
+//
+//  Created by MosquitoOuO on 2025-04-13.
+//
+
+import Testing
+@testable import FinalPrep
+
+struct FinalPrepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
