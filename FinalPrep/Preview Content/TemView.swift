d059922b0a5bbb055bb974a5ee79f3b05bb94259
Cyrus <cyrushoyat@gmail.com>
2025-04-13 22:25:26 -0700

    final excercise

---
+++ FinalPrep/Preview Content/TemView.swift
@@ -0,0 +1,18 @@
+//
+//  TemView.swift
+//  FinalPrep
+//
+//  Created by MosquitoOuO on 2025-04-13.
+//
+
+import SwiftUI
+
+struct TemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TemView()
+}
---
+++ FinalPrep/RowView.swift
@@ -0,0 +1,18 @@
+//
+//  RowView.swift
+//  FinalPrep
+//
+//  Created by MosquitoOuO on 2025-04-13.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RowView()
+}
